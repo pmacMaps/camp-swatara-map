6d95b935f3d8173f7471fed946e7f61be5300788
Patrick McKinney <pnmcartography@gmail.com>
2022-01-05 07:14:44 -0500

    get map attribution to update correctly sometimes
    after a few basemap changes, logic works as expected
    low priority, come back later

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,7 +6,6 @@ const windowWidth = window.innerWidth || document.documentElement.clientWidth ||
 const homeCoords = [40.501449, -76.362061];
 const initZoom = setInitialMapZoom(windowWidth);
 
-
 /*** Functions ***/
 // Reduce number of decimals for trail length in pop-up
 function reduceDecimalsTrailLength(data) {
@@ -41,19 +40,15 @@ function setPopupMaxWidth(windowWidth) {
 //  Change basemap
 const setBasemap = (selectedBasemap, basemap, webmap, topo, imagery) => {
     if (basemap) {
-       // remove topo map attribution > TODO
-       /*
-       if (basemap.options.name === 'topo') {
-        L.control.attribution.removeAttribution('OpenTopoMap');
-       }
-       */
-       // remove existing basemap
+        webmap.attributionControl.removeAttribution('OpenTopoMap');
+        // remove existing basemap
 	   webmap.removeLayer(basemap);
-	}
+    }
     if (selectedBasemap === 'imagery') {
         basemap = imagery;
 	} else if (selectedBasemap === 'topography') {
 	   basemap = topo;
+       webmap.attributionControl.addAttribution('OpenTopoMap');
 	}
     webmap.addLayer(basemap);
     // close basemap panel
@@ -79,7 +74,6 @@ const zoomHomeControl = L.Control.zoomHome({
 /*** Basemaps ***/
 // PEMA Imagery
 const pemaImagery = L.esri.tiledMapLayer({
-    name: 'imagery',
     url: ' https://imagery.pasda.psu.edu/arcgis/rest/services/pasda/PEMAImagery2018_2020/MapServer',
     detectRetina: true,
     attribution: 'Pennsylvania Emergency Management Agency'
@@ -87,13 +81,13 @@ const pemaImagery = L.esri.tiledMapLayer({
 
 // Open Topographic Map
 const openTopoMap = L.tileLayer('https://a.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    name: 'topo',
     detectRetina: true,
     attribution: 'OpenTopoMap'
 });
 
 // set inital basemap
-let basemap = pemaImagery.addTo(map);
+let basemap = pemaImagery;
+basemap.addTo(map);
 
 /*** Overlays ***/
 // Hiking Trails
