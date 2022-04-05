4f901459657d33eb315adbee28c85931b6eabb23
Patrick McKinney <pnmcartography@gmail.com>
2022-04-05 15:07:47 -0400

    prep for webpack transition

--- assets/js/constants.js
+++
@@ -1,9 +0,0 @@
-import { setInitialMapZoom } from './functions.js';
-
-// viewport
-export const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-// Map & Controls
-export const homeCoords = [40.501449, -76.362061];
-export const initZoom = setInitialMapZoom(windowWidth);
-// esri api key
-export const esriKey = 'AAPK56c13fae1a6d4407a5d392981b9b50d2kwxUZMYHbCl1MyifP-JufC7lrC9K2TvvL93oR25h20j0cjvoAro30x-p0O27jPwS';
