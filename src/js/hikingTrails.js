84fdf8af515c990271873fb53a0850a44bf94fa2
Patrick McKinney <pnmcartography@gmail.com>
2022-05-24 10:39:22 -0400

    update hiking trails style > not sure if it is working or not

--- src/js/hikingTrails.js
+++ src/js/hikingTrails.js
@@ -1,4 +1,5 @@
 import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import Color from "@arcgis/core/Color";
 
 // hiking trails data file
 // TODO: define json or geojson transformer to load geojson file from local system
@@ -15,6 +16,126 @@ const geojsonBlob = new Blob([JSON.stringify(data)], {
 // URL reference to geojson blob
 const geojsonUrl = URL.createObjectURL(geojsonBlob);
 */
+
+const trailsRenderer = {
+    type: "unique-value",
+    field: "Name",
+    defaultSymbol: { type: "simple-line" },
+    uniqueValueInfos: [
+        {
+            value: "Boundary Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(255,170,170)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Cat's Eye Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(255,85,0)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Cathedral Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(0,92,230)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Creek Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(197,0,255)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Frog Pond Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(200,20,43)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Grapevine Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(169,0,230)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Ground Hog Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(242,242,61)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Hemlock Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(56,168,0)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Mountain Side Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(0,255,197)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Nature Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(255,255,0)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Pool Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(255,255,115)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Power Line Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(76,230,0)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Rock Pile Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(0,92,230)"),
+            width: "3px"
+            }
+        },
+        {
+            value: "Tree House Trail",
+            symbol: {
+            type: "simple-line",
+            color: new Color("rgb(230,115,0)"),
+            width: "3px"
+            }
+        }
+    ]
+};
 
 export const hikingTrails = new GeoJSONLayer({
     copyright: 'Camp Swatara',
@@ -41,126 +162,5 @@ export const hikingTrails = new GeoJSONLayer({
             ]
         }]
     },
-    renderer: {
-        type: "unique-value",
-        field: "Name",
-        defaultSymbol: { 
-            type: "simple-line", 
-            color: '#fff' 
-        },
-        uniqueValueInfos: [
-            {
-                value: "Boundary Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(255,170,170)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Cat's Eye Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(255,85,0)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Cathedral Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(0,92,230)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Creek Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(197,0,255)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Frog Pond Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(200,20,43)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Grapevine Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(169,0,230)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Ground Hog Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(242,242,61)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Hemlock Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(56,168,0)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Mountain Side Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(0,255,197)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Nature Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(255,255,0)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Pool Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(255,255,115)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Power Line Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(76,230,0)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Rock Pile Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(0,92,230)",
-                width: "3px"
-                }
-            },
-            {
-                value: "Tree House Trail",
-                symbol: {
-                type: "simple-line",
-                color: "rgb(230,115,0)",
-                width: "3px"
-                }
-            }
-        ]
-    }
+    renderer: trailsRenderer
 });
--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,7 @@ import { hikingTrails } from './hikingTrails.js';
 esriConfig.apiKey = 'AAPK56c13fae1a6d4407a5d392981b9b50d2kwxUZMYHbCl1MyifP-JufC7lrC9K2TvvL93oR25h20j0cjvoAro30x-p0O27jPwS';
 
 mapView.when(function() {
-   webmap.addMany([mapOverlays.atTrail ,mapOverlays.usgsContours, hikingTrails]);
+   webmap.addMany([mapOverlays.atTrail , mapOverlays.usgsContours, hikingTrails]);
    mapView.ui.add(mapWidgets.homeWidget, "top-left");
    mapView.ui.add(mapWidgets.locateWidget, "top-left");
    mapView.ui.add(mapWidgets.scalebar, "bottom-left");
